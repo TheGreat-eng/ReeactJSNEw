a7d3ebad9e44c47e590caf61e1e4c857e0dbddfe
TheGreat <mirindano111@gmail.com>
2025-09-08 08:43:16 +0700

    31

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -9,10 +9,19 @@
 // }
 import './style.css'
 const MyComponent = () => {
+    const name = {
+        "name": "peter",
+        "age": 22,
+        "city": "New York"
+    }; // object
     return (
         <>
             <div className="my-component">My Component</div>
             <div className="child">Additional content</div>
+            <div className="name">Name: {name.name}</div>
+            <div className="age">Age: {name.age}</div>
+            <div className="city">City: {name.city}</div>
+            <div>Object : {JSON.stringify(name)}</div>
         </>
     );
 }
